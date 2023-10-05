b8ff66dee3f4b1b21e4bde2a464f8d40fc9d5331
chimekinglsey <onlinekingsley@gmail.com>
2023-10-05 22:59:11 +0100

    dcalculate the floor of a number using math.floor

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+0-Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function returns the sum of two floats"""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+2 - Basic annotations - floor
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """returns the floor of n"""
+    return math.floor(n)
