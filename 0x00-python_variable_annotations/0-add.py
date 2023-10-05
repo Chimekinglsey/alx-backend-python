465b4d862c07c5d4f44a6ebc4c37441963d320b9
chimekinglsey <onlinekingsley@gmail.com>
2023-10-05 22:22:56 +0100

    Basic annotation - add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+0-Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """This function returns the sum of two floats"""
+    return a + b
---
+++ README.md
@@ -0,0 +1,1 @@
+This directory serves as codebase for all python backend projects covered in ALXSE specialization
