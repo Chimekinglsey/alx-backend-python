585ade05812f19a7a4459932423c01031e3c9a0f
chimekinglsey <onlinekingsley@gmail.com>
2023-10-06 00:54:52 +0100

    annotating iterable objects

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+ 9. Let's duck type an iterable object
+"""
+from typing import List, Sequence, Iterable, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """returns duck or annotation for subscriptibles"""
+    return [(i, len(i)) for i in lst]
