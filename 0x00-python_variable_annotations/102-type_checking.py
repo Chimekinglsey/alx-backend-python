a9647f33b7d2786a9a13ff0f0fb2a83d9309e4ad
chimekinglsey <onlinekingsley@gmail.com>
2023-10-06 01:50:58 +0100

    Zoom an array based on provided zoom level

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
- 102. Type Checking
+102. Type Checking
 """
 from typing import Tuple, List, Any
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List[Any]:
-    """zooms and array based on provided zoom value"""
+    """zooms an array based on the provided zoom factor"""
     zoomed_in: List[Any] = [
         item for item in lst
-        for i in range(int(factor))
+        for _ in range(int(factor))
     ]
     return zoomed_in
 
