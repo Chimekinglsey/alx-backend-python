c887cd484e9cc3e3671dc7bd0caae8e131ca539d
chimekinglsey <onlinekingsley@gmail.com>
2023-10-09 19:41:27 +0100

    task_wait_random

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+3. task_await_random
+"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """returns a list of random floats """
+    wait_list: List[float] = []
+    for i in range(n):
+        result = await wait_random(max_delay)
+        wait_list.append(result)
+
+    for item in range(len(wait_list)):
+        for i in range(len(wait_list) - item - 1):
+            if wait_list[i] > wait_list[i + 1]:
+                wait_list[i], wait_list[i + 1] = wait_list[i + 1], wait_list[i]
+      
+    return wait_list
