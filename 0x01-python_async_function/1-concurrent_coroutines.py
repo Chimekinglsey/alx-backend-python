d84f017bee58800da55d349ea7b2025aa46bd3ff
chimekinglsey <onlinekingsley@gmail.com>
2023-10-09 17:10:21 +0100

    bubble sorted async

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+1. Execute  multiple coroutines at the same time with async
+"""
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """returns a list of random floats """
+    wait_list: List[float] = []
+    for i in range(n):
+        result = await wait_random(max_delay)
+        wait_list.append(result)
+
+    for item in range(len(wait_list)):
+        for i in range(len(wait_list) - item - 1):
+            if wait_list[i] > wait_list[i + 1]:
+                wait_list[i], wait_list[i + 1] = wait_list[i + 1], wait_list[i]
+                # tmp = wait_list[i + 1]
+                # wait_list[i + 1] = wait_list[i]
+                # wait_list[i] = tmp
+
+    return wait_list
