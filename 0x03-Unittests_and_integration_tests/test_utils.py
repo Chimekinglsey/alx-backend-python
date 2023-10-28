b74b4a7b7a59d44306e0655d8affc1f43901a5dc
chimekinglsey <onlinekingsley@gmail.com>
2023-10-29 00:18:13 +0100

    .

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 """
 This module contains unit tests for the
-    access_nested_map function in the utils module.
+access_nested_map function in the utils module.
 """
 import unittest
-from parameterized import parameterized, parameterized_class
+from parameterized import parameterized
 import utils
 from typing import (
-    Mapping,
-    Sequence,
-    Any,
-    Dict,
-    Callable,
+    Mapping,  # Type hint for a mapping type.
+    Sequence,  # Type hint for a sequence type.
+    Any,  # Type hint for any data type.
 )
 
 
@@ -19,13 +17,25 @@ class TestAccessNestedMap(unittest.TestCase):
     """Test class for AccessNestedMap"""
 
     @parameterized.expand([
-        ("depth_1_1", {"a": 1}, ("a",), 1),
-        ("depth_2_1", {"a": {"b": 2}}, ("a",), {'b': 2}),
-        ("depth_2_2", {"a": {"b": 2}}, ("a", "b"), 2),
+        ("depth_1_1", {"a": 1}, ("a",), 1),  # Test case 1
+        ("depth_2_1", {"a": {"b": 2}}, ("a",), {'b': 2}),  # Test case 2
+        ("depth_2_2", {"a": {"b": 2}}, ("a", "b"), 2),  # Test case 3
     ])
     def test_access_nested_map(self, name: str, map: Mapping[str, Any],
                                path: Sequence, expected: int) -> None:
-        """ Test the access_nested_map function """
+        """
+        Test the access_nested_map function.
+
+        Args:
+            name (str): A descriptive name for the test case.
+            map (Mapping[str, Any]): A mapping or nested map to test.
+            path (Sequence): A sequence representing the
+                path to access a value in the map.
+            expected (int): The expected result when accessing the nested map.
+
+        Returns:
+            None
+        """
         self.assertEqual(utils.access_nested_map(map, path), expected)
 
 
