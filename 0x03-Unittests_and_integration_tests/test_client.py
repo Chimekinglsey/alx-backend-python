b41929e4c7e125254f63203e1930e5d43efaa364
chimekinglsey <onlinekingsley@gmail.com>
2023-10-31 03:34:35 +0100

    Unit-test for GithubOrgClient.has_license.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -44,6 +44,16 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_pub.assert_called_once()
             mock_get_json.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+
+    ])
+    def test_has_license(self, nested_map, path, expected):
+        """Unit-test for GithubOrgClient.has_license."""
+        self.assertEqual(
+            GithubOrgClient.has_license(nested_map, path), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
