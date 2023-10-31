f7486f9699aa5729daddbdd2c1d70d34edc10fb6
chimekinglsey <onlinekingsley@gmail.com>
2023-10-31 05:38:48 +0100

    fixed docstring in get_payload method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -81,6 +81,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         }
 
         def get_payload(url):
+            """retrieves payload if exists"""
             if url in route_payload:
                 return Mock(**{'json.return_value': route_payload[url]})
             return HTTPError
