70a4e722bc43b7e212fa24f1ccb8e29f0e22bdaa
chimekinglsey <onlinekingsley@gmail.com>
2023-10-31 04:02:48 +0100

    """Unit-test for GithubOrgClient.has_license."""

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -52,7 +52,8 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_has_license(self, repo: Dict[str, Dict],
                          license_key: str, expected_result: bool) -> None:
         """Unit-test for GithubOrgClient.has_license."""
-        result = GithubOrgClient.has_license(repo, license_key)
+        org = GithubOrgClient('holberton')
+        result = org.has_license(repo, license_key)
         self.assertEqual(result, expected_result)
 
 
